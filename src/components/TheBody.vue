f854eea5fec7489a3fb3b7e1d74a81717aa8896c
jeremyimmanuel <jeremytandjung@icloud.com>
2021-04-25 18:31:20 -0700

    added proxy devserver for mocking api call + succesfully called an api

--- src/components/TheBody.vue
+++ src/components/TheBody.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="h-screen bg-indigo-200">
     <h1>The weather looks nice today!</h1>
+    {{ weatherCondition }}
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import { openWeatherApiKey } from "@/constants";
+
 export default {
   name: "TheBody",
+
+  data() {
+    return {
+      weatherCondition: "",
+    };
+  },
+
+  async created() {
+    const appid = openWeatherApiKey;
+    // const url = `/data/2.5/weather?q=Seattle&appid=${appid}`;
+    const url = "/data/2.5/weather";
+    const params = {
+      q: "Seattle",
+      appid,
+    };
+    const obj = await axios.get(url, {
+      params,
+    });
+
+    console.log("res", obj.data);
+    this.weatherCondition = obj.data.weather[0].description;
+  },
 };
 </script>
